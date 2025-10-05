83f5c9ece97d1b1976a77111304459e468eba9ba
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-10-05 20:40:24 +0600

    form validation

---
+++ JS Learning Topic/1 . JavaScript - Learn Everything 2025 video/EventListener/Form validation/script.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form")
+const nm = document.querySelector("#name")
+const right = document.querySelector("#right")
+const mail = document.querySelector("#mail")
+const mail_valid = document.querySelector("#mail_valid")
+const password = document.querySelector("#password")
+const pas_sms = document.querySelector("#pas_sms")
+
+
+form.addEventListener("submit", (dtls) => {
+    // console.dir()
+    dtls.preventDefault()
+    if (nm.value.length <= 2) {
+        right.style = "display: block; color: red;"        
+    }else{
+        right.style = "display: none;"
+    }
+
+
+
+    let check = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+    let test = check.test(mail.value)
+    if(test){
+        mail_valid.style = "display : none;"
+    }else{
+        mail_valid.style = "display : block; color: red;"
+    }
+
+
+    
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let password_Check = passwordRegex.test(password.value) 
+    
+    if(password_Check){
+        pas_sms.style = "display : none;"
+    }else{
+        pas_sms.style = "display : block; color: red;"
+    }
+})
